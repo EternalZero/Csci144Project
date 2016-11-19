3f5b46ecb8cc168d98f34e8e2695e525e8bea10e
EternalZero <jray12332@gmail.com>
2016-11-19 15:53:58 -0800

    Cpp File, and Test price list
    Checking if able to read file

---
+++ proj.cpp
@@ -0,0 +1,25 @@
+
+// reading a text file
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main () {
+  string line;
+  ifstream myfile ("data.txt");
+  if (myfile.is_open())
+  {
+    while (! myfile.eof())
+    {
+      getline(myfile, line);
+     if (!line.empty())
+      {
+        cout<<line<<endl;
+      }}
+    myfile.close();
+  }
+
+  else cout << "Unable to open file";
+  return 0;
+}
